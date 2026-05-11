fd85f91f49414a9cea663346c5e3fe7483c151a0
JunRui Fang <158279847+Tomabsolute@users.noreply.github.com>
2026-05-11 17:32:45 +0800

    Delete trainer_2 directory

--- trainer_2/__init__.py
+++
@@ -1,4 +0,0 @@
-from trainer_2.drpo_config import DRPOConfig
-from trainer_2.drpo_trainer import DRPOTrainer
-from trainer_2.drpo_utils import GPMwithRewardNetwork, estDPOStylePipeline, BTRewardNetwork
-__all__ = ["DRPOConfig","DRPOTrainer","GPMwithRewardNetwork", "estDPOStylePipeline", "BTRewardNetwork"]
